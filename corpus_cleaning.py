b8c444605b76acbfcce2a36f1dab0c0abcce7670
Lasse Kohlmeyer <l.kohlmeyer@gmx.net>
2020-05-22 13:35:17 +0200

    wrote cleaning method to put authors in list

--- corpus_cleaning.py
+++ corpus_cleaning.py
@@ -1,6 +1,7 @@
 import os
 
 from utils import DataHandler, ConfigLoader
+import numpy as np
 
 
 def create_new_filepath_uncleaned(file_path):
@@ -51,14 +52,59 @@ def cleaning_bundestag(config, overwrite=True):
     DataHandler.save_corpus(corpus, config["corpora"]["bundestag_corpus"])
 
 
+def cleaning_authors(config, overwrite=False):
+    corpus_names = [
+        # "bundestag_corpus",
+        # "sustainability_corpus",
+        "abstract_corpus"
+    ]
+    wlc = 0
+    m_a = 0
+    s_a = 0
+    for corpus_name in corpus_names:
+        corpus = DataHandler.load_corpus(config["corpora"][corpus_name])
+        for d in corpus:
+            if d.author:
+                if isinstance(d.author, float) and np.isnan(d.author):
+                    d.author = None
+                else:
+                    if corpus_name == "bundestag_corpus":
+                        authors = [d.author]
+                    elif corpus_name == "sustainability_corpus":
+                        authors = [a.strip() for a in d.author.split(',')]
+                        authors = [f'{j}. {i}' for i, j in zip(authors[::2], authors[1::2])]
+                    else:
+                        if d.language != "English":
+                            wlc += 1
+                            continue
+                        authors = [a.strip() for a in d.author.split(',')]
+                        authors = [f'{j}. {i}' for i, j in zip(authors[::2], authors[1::2])]
+                        if len(authors) > 1:
+                            m_a += 1
+                            print(d.author, authors)
+                        else:
+                            s_a += 1
+                    d.author = authors
+
+        if not overwrite:
+            os.rename(src=config["corpora"][corpus_name], dst=create_new_filepath_uncleaned(config["corpora"][corpus_name]))
+
+        DataHandler.save_corpus(corpus, config["corpora"][corpus_name])
+    print(wlc, m_a, s_a)
+
+
+
+
 def main():
     # load configuration parameters from config file
     config = ConfigLoader.get_config()
 
     # deletes unusable documents and replaces date with year int
-    cleaning_abstracts(config, overwrite=False)
-    cleaning_sustainability(config, overwrite=False)
-    cleaning_bundestag(config, overwrite=False)
+    # cleaning_abstracts(config, overwrite=False)
+    # cleaning_sustainability(config, overwrite=False)
+    # cleaning_bundestag(config, overwrite=False)
+
+    cleaning_authors(config, overwrite=False)
 
 
 if __name__ == '__main__':
