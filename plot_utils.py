cad9e980383285f30cf0cab5039ef9aff5dc9323
Lasse Kohlmeyer <l.kohlmeyer@gmx.net>
2020-06-03 13:23:42 +0200

    documented plotting functions

--- plot_utils.py
+++ plot_utils.py
@@ -1,17 +1,32 @@
-from typing import Callable, List, Union
+from typing import List, Union
 from matplotlib import pyplot as plt
 
 
 def simple_bar_histogram(bin_data: List[Union[int, str]], count_data: List[int]):
+    """
+    plots a simple bar histogram
+    :param bin_data: data for x axis
+    :param count_data: data for y axis
+    :return:
+    """
     plt.bar(x=bin_data, height=count_data, color='lawngreen', align='center', alpha=0.5)
     plt.show()
 
 
-def multi_bar_histogram(multiple_bin_data: List[List[Union[int, str]]],
+def multi_bar_histogram(multiple_bin_data: List[List[int]],
                         multiple_count_data: List[List[int]],
                         labels: List[str],
                         normalize: bool = True,
                         corpus_lengths: List[int] = None):
+    """
+    plots a multi bar histogram from different datasources
+    :param multiple_bin_data: numeric data of different series organized in lists for x axis
+    :param multiple_count_data: count data of different series organized in lists for y axis
+    :param labels: labels for the different series
+    :param normalize: normalizes the count data by sum of series or other provided values with corpus_lengths
+    :param corpus_lengths: provided corpus lengths for normalization
+    :return:
+    """
     c = []
     if normalize:
         for i, count_data in enumerate(multiple_count_data):
