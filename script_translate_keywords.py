d4b64378afc158fc8e769d24607f0aafe1e4c297
Lasse Kohlmeyer <l.kohlmeyer@gmx.net>
2020-07-02 12:09:33 +0200

    cleaned file

--- script_translate_keywords.py
+++ script_translate_keywords.py
@@ -1,47 +1,28 @@
 import argparse
-from collections import namedtuple
-
-from utils import ConfigLoader, Corpus, Language
-
+from utils import ConfigLoader
 import json
 
-def main():
-    # parser = argparse.ArgumentParser(description='Extracts keywords for given algorithm on given corpora')
-    # parser.add_argument('-a', '--algorithm', help='Algorithm to use like rake or tfidf', default="rake")
-    # parser.add_argument('-c', '--corpora', help='Corpora to annotate as list', nargs='+',
-    #                     default=['state_of_the_union'])
-    # parser.add_argument('-t', '--translate', help='Translate keywords', action='store_true')
-    # args = vars(parser.parse_args())
-    #
-    # config = ConfigLoader.get_config()
-    #
-    # #  remove and use actual args
-    # chosen_corpora = [
-    #     # 'state_of_the_union',
-    #     'bundestag', 'abstract', 'sustainability'
-    # ]  # args['corpora']
-    #
-    # PathMetaData = namedtuple('PathMetaData', 'path corpus_name language')
-    # paths_and_meta_data = [
-    #     PathMetaData(config["corpora"]["bundestag_corpus"], "bundestag", Language.DE),
-    #     PathMetaData(config["corpora"]["abstract_corpus"], "abstract", Language.EN),
-    #     PathMetaData(config["corpora"]["sustainability_corpus"], "sustainability", Language.EN),
-    #     PathMetaData(config["corpora"]["state_of_the_union_corpus"], "state_of_the_union", Language.EN),
-    #     PathMetaData(config["corpora"]["united_nations_corpus"], "united_nations", Language.EN)
-    # ]
-    # paths_and_meta_data = [path_meta for path_meta in paths_and_meta_data if path_meta.corpus_name in chosen_corpora]
 
-    paths = ['data/state_of_the_union_corpus_rake_keywords.json']
+def main():
+    parser = argparse.ArgumentParser(description='Translates keywords in keyword files of given paths')
+    parser.add_argument('-p', '--paths', help='Paths of keyword files to translate', nargs='+',
+                        default=['data/state_of_the_union_corpus_rake_keywords.json'])
+    args = vars(parser.parse_args())
+
+    config = ConfigLoader.get_config()
+    paths = args['paths']
+
     for path in paths:
         with open(path) as f:
             data = json.load(f)
-            for doc_id, keywords in data:
-                en_translation = keywords["english_translation"]
-                ger_translation = keywords["german_translation"]
-                if en_translation is None:
-                    pass
-                if ger_translation is None:
-                    pass
+            for doc_id, keywords in data.items():
+                for keyword in keywords:
+                    en_translation = keyword["english_translation"]
+                    ger_translation = keyword["german_translation"]
+                    if en_translation is None:
+                        pass
+                    if ger_translation is None:
+                        pass
 
 
 if __name__ == '__main__':
