8a6e93e9e39e03e57f3606a781e858e85f2091e8
Lasse Kohlmeyer <l.kohlmeyer@gmx.net>
2020-07-10 19:19:46 +0200

    added script for file fix

---
+++ script_fix_corrupted_keyword_file.py
@@ -0,0 +1,21 @@
+import json
+from collections import defaultdict
+
+path = "data/bundestag_corpus_yake_keywords_yearwise.json"
+with open(path, encoding='utf-8') as f:
+    data = json.load(f)
+
+res = defaultdict(list)
+for doc_id, keywords in data.items():
+    for keyword in keywords:
+        if isinstance(keyword["german_translation"], list):
+            keyword["german_translation"] = keyword["german_translation"][0]
+
+        if isinstance(keyword["english_translation"], list):
+            keyword["english_translation"] = keyword["english_translation"][0]
+
+        res[doc_id].append(keyword)
+
+
+with open(path, "w", encoding='utf-8') as f:
+    json.dump(data, f, indent=1, ensure_ascii=True)
